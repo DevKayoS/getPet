8928364bd2b9c3c1bf09aca3092eb15b19960e4d
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-06-11 12:19:19 -0300

    build: initial setup for the frontend and structuring paths

--- api/index.ts
+++ api/index.ts
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 
 // Solve CORS
-app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
+app.use(cors({credentials: true, origin: 'http://localhost:5173'}))
 
 // Public folder for images
 app.use(express.static('public'))
---
+++ frontend/src/App.tsx
@@ -0,0 +1,7 @@
+
+export function App() {
+  return(
+    <div className="text-2xl text-blue-300">Get a Pet</div>
+  )
+}
+
