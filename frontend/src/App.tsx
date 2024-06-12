b4f8829ea18a1a4451e765a6b703a3b224af36c2
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-06-12 12:02:14 -0300

    feat: add context for the aplication

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,19 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "./components/header";
 import { Footer } from "./components/footer";
+// context
+import { UserProvider } from "./context/UserContext";
 
 export function App() {
   return(
     <div>
+      <UserProvider>
       <Header/>
       <div className="w-full m-auto min-h-screen px-8 py-4">
         <Outlet/>
       </div>
       <Footer/>
+      </UserProvider>
     </div>
   )
 }
