3c78124bd070a5f890df1db7f311ad6ece5e14ab
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-06-13 22:19:43 -0300

    feat: bug with updating data

--- frontend/src/routes/User/Profile.tsx
+++ frontend/src/routes/User/Profile.tsx
@@ -1,15 +1,10 @@
 import { Input } from "@/components/form/input";
 import { useEffect, useState } from "react";
-import { Toaster } from "sonner";
+import { Toaster, toast } from "sonner";
 import api from '../../utils/api'
 
 export function Profile(){
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  const [phone, setPhone] = useState("")
-  const [ password, setPassword] = useState("")
   const [image, setImage] = useState("")
-  const [ confirmPassword, setConfirmPassword] = useState("")
   const [user,setUser] = useState({})
 
   const [token] = useState(localStorage.getItem('token') || '')
@@ -25,7 +20,36 @@ export function Profile(){
   },[token])
   
  
-  function onFileChange(e){
+  function onFileChange(e: { target: { value: any; files: any[]; }; }){
+    setUser({...user, [e.target.value]: e.target.files[0]})
+  }
+  function handleOnChange(e: { target: { value: any; }; }){
+    setUser({...user, [e.target.value]: e.target.value})
+  }
+
+  async function handleSubmit(e: { preventDefault: () => void; }){
+    e.preventDefault()
+
+    const formData =  new FormData()
+
+    const userFormData = Object.keys(user).forEach((key) => formData.append(key, user[key])
+    )
+
+    console.log(formData)
+    console.log(user.name)
+    
+    const data =  await api.patch(`/users/edit/${user._id}`, formData, {
+      headers: {
+        Authorization: `Bearer ${JSON.parse(token)}`,
+        'Content-type': 'multipart/form-data'
+      }
+    }).then((response)=> {
+      toast.success('Dados atualizado com sucesso!')
+      return response.data
+    }).catch((err)=> {
+      const message = err.response.data.message
+      toast.error(`Algo deu errado: ${message}`)
+    })
 
   }
  
@@ -33,12 +57,11 @@ export function Profile(){
 
     <div className="text-2xl text-slate-100">
       <p>preview Imagem</p>
-      <form className="flex flex-col items-center justify-center m-auto max-w-xl bg-zinc-600/20 gap-5 rounded-lg shadow-2xl shadow-black p-4">
+      <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center m-auto max-w-xl bg-zinc-600/20 gap-5 rounded-lg shadow-2xl shadow-black p-4">
       <Input
           text="Imagem"
           type="file"
           name="image"
-          
           handleOnChange={onFileChange} 
           multiple={undefined}       
            />
@@ -48,7 +71,7 @@ export function Profile(){
           name="name"
           placeholder="Digite o seu nome"
           value={user.name}
-          handleOnChange={(e)=> setName(e.target.value)} 
+          handleOnChange={handleOnChange} 
           multiple={undefined}       
            />
         <Input
@@ -57,7 +80,7 @@ export function Profile(){
           name="email"
           placeholder="Digite o seu e-mail"
           value={user.email} 
-          handleOnChange={(e)=> setEmail(e.target.value)} 
+          handleOnChange={handleOnChange} 
           multiple={undefined}       
            />
         <Input
@@ -66,7 +89,7 @@ export function Profile(){
           name="phone"
           placeholder="Digite o seu telefone"
           value={user.phone}
-          handleOnChange={(e)=> setPhone(e.target.value)} 
+          handleOnChange={handleOnChange} 
           multiple={undefined}       
            />
         <Input
@@ -74,7 +97,7 @@ export function Profile(){
           type="password"
           name="password"
           placeholder="Digite a sua senha"
-          handleOnChange={(e)=> setPassword(e.target.value)} 
+          handleOnChange={handleOnChange} 
           multiple={undefined}       
            />
         <Input
@@ -82,7 +105,7 @@ export function Profile(){
           type="password"
           name="confirmPassword"
           placeholder="Confirme a sua senha"
-          handleOnChange={(e)=> setConfirmPassword(e.target.value)} 
+          handleOnChange={handleOnChange} 
           multiple={undefined}       
            />
           <input type="submit" value="Cadastrar" className=" bg-zinc-500/20 rounded-md shadow-md  shadow-black w-full h-12 hover:bg-sky-900 cursor-pointer"/>
