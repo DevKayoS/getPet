4941c4788b8daa5f0b24774de55de28f3da20e1b
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-06-20 12:42:47 -0300

    feat: added pushing pet data for show in page details

--- frontend/src/routes/Pets/petDetails.tsx
+++ frontend/src/routes/Pets/petDetails.tsx
@@ -1,7 +1,36 @@
+import { IPet } from "@/interface/IPet"
+import api from "@/utils/api"
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import { toast } from "sonner"
+import { Toaster } from "@/components/ui/sonner"
+
 export function PetDetails(){
+  const [pet, setPet] = useState<IPet>({
+    name: "",
+    age: "",
+    coat: "",
+    weight: "",
+    images: [],
+  })
+  const {id} = useParams()
+  const [token] = useState(localStorage.getItem('token')|| '')
+  useEffect(()=>{
+   api.get(`/pets/${id}`).then((response)=>{
+    setPet(response.data.pet)
+   }).catch((err)=>{
+    toast.error('Algo deu errado',{
+      description: `${err.response.data.message}`
+    })
+    return err.response.data
+   }) 
+  }, [id])
+  console.log(pet)
+
   return(
     <div>
-      <h1 className="text-slate-50">teste</h1>
+      <h1 >{pet.name}</h1>
+      <Toaster richColors/>
     </div>
   )
 }
